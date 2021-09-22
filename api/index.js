07706ccceefed4865898dde92a00caeb01572bd4
Arm Supakorn <arm.supakorn@bitkub.com>
2021-09-22 23:14:10 +0700

    fix: call to apps script endpoint with x-www-form-urlencoded

--- api/index.js
+++ api/index.js
@@ -1,32 +1,49 @@
 const express = require("express");
 const line = require("@line/bot-sdk");
 const axios = require("axios");
+const qs = require("qs");
 
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: "xxx", // add your channel access token
+  channelSecret: "xxx", // add your channel secret
 };
+
+const APPS_SCRIPT_URL = "xxx"; // add your google app script url
 
 const app = express();
 
 app.get("/api", (req, res) => res.send("Hello World!"));
 
 app.post("/api/webhook", line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent)).then((result) =>
-    res.json(result)
-  );
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
 const client = new line.Client(config);
 async function handleEvent(event) {
+  console.log("event", event);
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
 
   try {
-    const data = await axios.post(process.env.GOOGLE_APPS_SCRIPT_URL, {
-      text: event.message.text,
-    });
+    const data = await axios.post(
+      APPS_SCRIPT_URL,
+      qs.stringify({
+        text: event.message.text,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    console.log(data.data);
 
     return client.replyMessage(event.replyToken, {
       type: "text",
